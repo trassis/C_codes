8f125e6143fa5c726931c4b053f1499099f80616
trassis <107087573+trassis@users.noreply.github.com>
2022-06-22 21:10:32 -0300

    Update dynamic_array.c

--- structures/dynamic_array.c
+++ structures/dynamic_array.c
@@ -3,23 +3,19 @@
 typedef struct {
     int index; 
     int size;
-    int *array;
+    int *data;
 } vector;
 
-void init_vector(vector *v, int size){
+void init_vect(vector *v, int size){
     v->index = 0;
     v->size = size;
-    v->array = malloc(size*sizeof(int));
+    v->data = malloc(size*sizeof(int));
 }
 
-void free_vector(vector *v){
-    free(v->array);
-}
-
-void vec_append(vector *v, int value){
+void append_vect(vector *v, int value){
     if(v->index == v->size){
-        v->array = realloc(v->array, 2*v->size*sizeof(int));
+        v->data = realloc(v->data, 2*v->size*sizeof(int));
         v->size *= 2;
     }
-    v->array[v->index++] = value;
+    v->data[v->index++] = value;
 }
